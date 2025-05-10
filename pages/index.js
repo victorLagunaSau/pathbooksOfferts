bc1b82d51e0327cb5d17ecfec1c716ce7650af18
victorLagunaSau <viktor.laguna@gmail.com>
2025-05-10 18:08:51 -0500

    Divicion

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import SeoHead from "../components/SeoHead";
 import Layout from "../components/Layout/Layout";
 import Home001 from "../components/Home001";
-
+import ModuleDivision01 from "../components/ModuleDivision01";
 
 export default function Home() {
     return (
@@ -9,6 +9,7 @@ export default function Home() {
             <SeoHead title='Maestros-Pathbooks'/>
             <Layout>
                 <Home001/>
+                <ModuleDivision01/>
 
             </Layout>
         </>
